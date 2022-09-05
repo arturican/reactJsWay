05d36f58b11a60520753222dc4bcad4fb3ba2a24
arturican <arturican@gmail.com>
2022-09-05 14:25:20 +0300

    Добавил компоненты Header и Technologies по уроку 07. Уроки React JS (что такое Компонента)

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h2>Hello, samurai! Let's go!</h2>
-        <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-            <li>React</li>
-        </ul>
+    <div>
+     <Header />
+     <Technologies />
     </div>
   );
 }
 
+const Technologies = () => {
+    return (
+        <div>
+            <h2>Hello, samurai! Let's go!</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+
+}
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">New Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+
+}
 export default App;
