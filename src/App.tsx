df130b5a88cffa55eb32adff9bbc0d594c3ddf51
arturican <arturican@gmail.com>
2022-09-05 13:54:25 +0300

    Посмотрел урок(06. Уроки React JS (index.js, App, JSX)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <h2>Hello, samurai! Let's go!</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
     </div>
   );
 }
