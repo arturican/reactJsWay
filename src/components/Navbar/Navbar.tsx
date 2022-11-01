242bd0c9f3cdd4ef0509369d68b039207332e4a2
arturican <arturican@gmail.com>
2022-11-01 11:55:50 +0300

    добавили Navlink и стили для активных ссылок

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div><a href='/message'>Message</a></div>
-            <div><a>News</a></div>
-            <div><a href='/profile'>Profile</a></div>
-            <div><a>Settings</a></div>
-            <div><a>Music</a></div>
-            <div><a href='/dialogs'>Dialogs</a></div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/message' activeClassName={s.active}>Message</NavLink>
+            </div>
+            <div className={s.item}>
+                <a>News</a>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/profile' activeClassName={s.active}>Profile</NavLink></div>
+            <div className={s.item}>
+                <a>Settings</a>
+            </div>
+            <div className={s.item}>
+                <a>Music</a>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/dialogs' activeClassName={s.active}>Dialogs</NavLink>
+            </div>
         </nav>
     )
 }
