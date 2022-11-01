e19315743fad111d4dc7bedcbf3d178a97618db0
arturican <arturican@gmail.com>
2022-11-01 19:46:11 +0300

    изменил dialogs и добавил стилей

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to={'Artur'}>Artur</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    Dilpopo
+                </div>
+                <div className={s.dialog}>
+                    Arturican
+                </div>
+                <div className={s.dialog}>
+                    Vasya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Как дела?</div>
+                <div className={s.message}>Что нового?</div>
+            </div>
         </div>
     )
 }
