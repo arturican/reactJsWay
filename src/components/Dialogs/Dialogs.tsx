c7ebc27f2d86e6cd98f42745032dfcc7febb44e2
arturican <arturican@gmail.com>
2022-11-02 08:48:52 +0300

    добавил props в Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,28 +3,40 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 
+type DialogsItemType = {
+    name: string,
+    id: number
+}
+
+ const DialogsItem = (props:DialogsItemType) => {
+    return (
+    <div className={s.dialog + ' ' + s.active}>
+        <NavLink to={'dialogs/' + props.id}>{props.name}</NavLink>
+    </div>
+    )
+}
+
+type MessageType = {
+    message: string
+}
+
+const Message = (props: MessageType) =>{
+    return  <div className={s.message}>{props.message}</div>
+}
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to='dialogs/1'>Artur</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='dialogs/2'>Dilpopo</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='dialogs/3'>Arturican</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='dialogs/4'>Vasya</NavLink>
-                </div>
+                <DialogsItem name={'Artur'} id={1}/>
+                <DialogsItem name={'Dilpopo'} id={2}/>
+                <DialogsItem name={'Arturican'} id={3}/>
+                <DialogsItem name={'Vasya'} id={4}/>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>Как дела?</div>
-                <div className={s.message}>Что нового?</div>
+                <Message message={'Hi'}/>
+                <Message message={'Как дела?'}/>
+                <Message message={'Что нового?'}/>
             </div>
         </div>
     )
